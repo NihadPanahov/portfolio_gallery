0707ee9cc399e4629bddb3f71e4e0de53a4f0e16
Nihad_Panahov <nihadpanah03@gmail.com>
2023-08-21 14:37:36 +0400

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,402 @@
+import React from "react";
+
+import Nihad_Logo from "./gallery/NİHAD_LOGO.png";
+
+import image1 from "./gallery/Agency1.PNG";
+import image2 from "./gallery/Agency2.PNG";
+import image3 from "./gallery/Agency3.PNG";
+import image4 from "./gallery/Agency4.PNG";
+import image5 from "./gallery/Bioxlabb.PNG";
+import image6 from "./gallery/Blog1.PNG";
+import image7 from "./gallery/Blog2.PNG";
+import image8 from "./gallery/Blog3.PNG";
+import image9 from "./gallery/Blog4.PNG";
+import image10 from "./gallery/Blog5.PNG";
+import image11 from "./gallery/Blogar1.PNG";
+import image12 from "./gallery/Blogar2.PNG";
+import image13 from "./gallery/Blogar3.PNG";
+import image14 from "./gallery/Blogar4.PNG";
+import image15 from "./gallery/Blogar5.PNG";
+import image16 from "./gallery/Blogar6.PNG";
+import image17 from "./gallery/Blogar7.PNG";
+import image18 from "./gallery/Blogar8.PNG";
+import image19 from "./gallery/Blogar9.PNG";
+import image20 from "./gallery/Blogar10.PNG";
+import image22 from "./gallery/Blogar12.PNG";
+import image23 from "./gallery/Blogar13.PNG";
+import image24 from "./gallery/Blogar14.PNG";
+import image25 from "./gallery/Blogar15.PNG";
+import image26 from "./gallery/Blogar16.PNG";
+import image27 from "./gallery/Blogar17.PNG";
+import image28 from "./gallery/Blogar18.PNG";
+import image29 from "./gallery/Blogar19.PNG";
+import image30 from "./gallery/Blogar20.PNG";
+import image31 from "./gallery/Blogar21.PNG";
+import image32 from "./gallery/CarDealer1.PNG";
+import image33 from "./gallery/CarDealer2.PNG";
+import image34 from "./gallery/CarDealer3.PNG";
+import image35 from "./gallery/CarDealer4.PNG";
+import image36 from "./gallery/CarDealer5.PNG";
+import image37 from "./gallery/CarDealer6.PNG";
+import image38 from "./gallery/CarDealer7.PNG";
+import image39 from "./gallery/CarDealer8.PNG";
+import image40 from "./gallery/CarDealer9.PNG";
+import image41 from "./gallery/CarDealer10.PNG";
+import image42 from "./gallery/CarDealer11.PNG";
+import image43 from "./gallery/CarDealer12.PNG";
+import image44 from "./gallery/CarDealer13.PNG";
+import image45 from "./gallery/CarDealer14.PNG";
+import image46 from "./gallery/CarDealer15.PNG";
+import image47 from "./gallery/CarDealer16.PNG";
+import image48 from "./gallery/CarDealer17.PNG";
+import image49 from "./gallery/CarDealer18.PNG";
+import image50 from "./gallery/CarDealer19.PNG";
+import image51 from "./gallery/CarDealer20.PNG";
+import image52 from "./gallery/CarDealer21.PNG";
+import image53 from "./gallery/CarDealer22.PNG";
+import image54 from "./gallery/Cloud1.PNG";
+import image55 from "./gallery/Cloud2.PNG";
+import image56 from "./gallery/Cloud3.PNG";
+import image57 from "./gallery/Cloud4.PNG";
+import image58 from "./gallery/Cloud5.PNG";
+import image59 from "./gallery/Cloud6.PNG";
+import image60 from "./gallery/Cofeecafe1.PNG";
+import image61 from "./gallery/Cofeecafe2.PNG";
+import image62 from "./gallery/Datanest1.PNG";
+import image63 from "./gallery/Datanest2.PNG";
+import image64 from "./gallery/Datanest3.PNG";
+import image65 from "./gallery/Datanest4.PNG";
+import image66 from "./gallery/Edash.PNG";
+import image67 from "./gallery/Elitavista1.PNG";
+import image68 from "./gallery/Elitavista2.PNG";
+import image69 from "./gallery/Elitavista3.PNG";
+import image70 from "./gallery/Elitavista4.PNG";
+import image71 from "./gallery/Elitavista5.PNG";
+import image72 from "./gallery/Episodes.PNG";
+import image73 from "./gallery/FB_login_duplicate1.PNG";
+import image74 from "./gallery/FB_signup_duplicate2.PNG";
+import image75 from "./gallery/Fiorello1.PNG";
+import image76 from "./gallery/Fiorello2.PNG";
+import image77 from "./gallery/Fiorello3.PNG";
+import image78 from "./gallery/Fiorello4.PNG";
+import image79 from "./gallery/Fiorello5.PNG";
+import image80 from "./gallery/Fiorello6.PNG";
+import image81 from "./gallery/Fiorello7.PNG";
+import image82 from "./gallery/Fiorello8.PNG";
+import image83 from "./gallery/Fiorello9.PNG";
+import image84 from "./gallery/Fiorello10.PNG";
+import image85 from "./gallery/Fiorello11.PNG";
+import image86 from "./gallery/Fiorello12.PNG";
+import image87 from "./gallery/Fiorello13.PNG";
+import image88 from "./gallery/Fiorello14.PNG";
+import image89 from "./gallery/Fiorello15.PNG";
+import image90 from "./gallery/Fiorello16.PNG";
+import image91 from "./gallery/Fiorello17.PNG";
+import image92 from "./gallery/Fiorello18.PNG";
+import image93 from "./gallery/Furniture_canvas.PNG";
+import image94 from "./gallery/Imroz1.PNG";
+import image95 from "./gallery/Imroz2.PNG";
+import image96 from "./gallery/Imroz3.PNG";
+import image97 from "./gallery/Imroz4.PNG";
+import image98 from "./gallery/Local_business1.PNG";
+import image99 from "./gallery/Local_business2.PNG";
+import image100 from "./gallery/Local_business3.PNG";
+import image101 from "./gallery/Merch.PNG";
+import image102 from "./gallery/Movflx.PNG";
+import image103 from "./gallery/Multimart1.PNG";
+import image104 from "./gallery/Multimart2.PNG";
+import image105 from "./gallery/Multimart3.PNG";
+import image106 from "./gallery/Pixlab.PNG";
+import image107 from "./gallery/Podcast1.PNG";
+import image108 from "./gallery/Podcast2.PNG";
+import image109 from "./gallery/Podcast3.PNG";
+import image110 from "./gallery/Podcast4.PNG";
+import image111 from "./gallery/Report1.PNG";
+import image112 from "./gallery/Report2.PNG";
+import image113 from "./gallery/Sante.PNG";
+import image114 from "./gallery/Starbelly1.PNG";
+import image115 from "./gallery/Starbelly2.PNG";
+import image116 from "./gallery/Starbelly3.PNG";
+import image117 from "./gallery/Starbelly4.PNG";
+import image118 from "./gallery/Starbelly5.PNG";
+import image119 from "./gallery/TT1.1.PNG";
+import image120 from "./gallery/TT1.2.PNG";
+import image121 from "./gallery/TT1.3.PNG";
+import image122 from "./gallery/TT2.1.PNG";
+import image123 from "./gallery/TT2.2.PNG";
+import image124 from "./gallery/TT2.3.PNG";
+import image125 from "./gallery/TT2.4.PNG";
+import image126 from "./gallery/TT3.1.PNG";
+import image127 from "./gallery/TT3.2.PNG";
+import image128 from "./gallery/TT4.1.PNG";
+import image129 from "./gallery/TT5.1.PNG";
+import image130 from "./gallery/TT6.1.PNG";
+import image131 from "./gallery/TT7.1.PNG";
+import image132 from "./gallery/TT7.2.PNG";
+import image133 from "./gallery/TT8.1.PNG";
+import image134 from "./gallery/TT8.2.PNG";
+import image135 from "./gallery/TT9.1.PNG";
+import image136 from "./gallery/TT10.1.PNG";
+import image137 from "./gallery/TT10.2.PNG";
+import image138 from "./gallery/TT10.3.PNG";
+import image139 from "./gallery/TT11.1.PNG";
+import image140 from "./gallery/TT12.1.PNG";
+import image141 from "./gallery/TT13.1.PNG";
+import image142 from "./gallery/TT13.2.PNG";
+
+//
+//
+//
+
+const imageLinks = [
+  "https://nihadpanahov.github.io/travel-react-tailwind/", 
+  "https://nihadpanahov.github.io/travel-react-tailwind/", 
+  "https://nihadpanahov.github.io/travel-react-tailwind/", 
+  "https://nihadpanahov.github.io/travel-react-tailwind/", 
+  "https://nihadpanahov.github.io/travel-react-tailwind/", 
+  "https://nihadpanahov.github.io/data-finance-yt/", 
+  "https://nihadpanahov.github.io/data-finance-yt/", 
+  "https://nihadpanahov.github.io/data-finance-yt/", 
+  "https://nihadpanahov.github.io/data-finance-yt/", 
+  "https://nihadpanahov.github.io/cloud-app-react/", 
+  "https://nihadpanahov.github.io/cloud-app-react/", 
+  "https://nihadpanahov.github.io/cloud-app-react/", 
+  "https://nihadpanahov.github.io/cloud-app-react/", 
+  "https://nihadpanahov.github.io/cloud-app-react/", 
+  "https://nihadpanahov.github.io/cloud-app-react/", 
+  "https://nihadpanahov.github.io/final_trendtrove/", 
+  "https://nihadpanahov.github.io/final_trendtrove/", 
+  "https://nihadpanahov.github.io/final_trendtrove/", 
+  "https://nihadpanahov.github.io/final_trendtrove/", 
+  "https://nihadpanahov.github.io/final_trendtrove/", 
+  "https://nihadpanahov.github.io/final_trendtrove/", 
+  "https://nihadpanahov.github.io/final_trendtrove/", 
+  "https://nihadpanahov.github.io/final_trendtrove/", 
+  "https://nihadpanahov.github.io/final_trendtrove/", 
+  "https://nihadpanahov.github.io/final_trendtrove/", 
+  "https://nihadpanahov.github.io/final_trendtrove/", 
+  "https://nihadpanahov.github.io/final_trendtrove/", 
+  "https://nihadpanahov.github.io/final_trendtrove/", 
+  "https://nihadpanahov.github.io/final_trendtrove/", 
+  "https://nihadpanahov.github.io/final_trendtrove/", 
+  "https://nihadpanahov.github.io/final_trendtrove/", 
+  "https://nihadpanahov.github.io/final_trendtrove/", 
+  "https://nihadpanahov.github.io/final_trendtrove/", 
+  "https://nihadpanahov.github.io/final_trendtrove/", 
+  "https://nihadpanahov.github.io/final_trendtrove/", 
+  "https://nihadpanahov.github.io/final_trendtrove/", 
+  "https://nihadpanahov.github.io/final_trendtrove/", 
+  "https://nihadpanahov.github.io/final_trendtrove/", 
+  "https://nihadpanahov.github.io/final_trendtrove/", 
+
+  // Diğer resimler için bağlantılar
+];
+
+const App = () => {
+  const images = [
+    image67,
+    image68,
+    image69,
+    image70,
+    image71,
+
+    image62,
+    image63,
+    image64,
+    image65,
+
+    image54,
+    image55,
+    image56,
+    image57,
+    image58,
+    image59,
+
+    image119,
+    image120,
+    image121,
+    image122,
+    image123,
+    image124,
+    image125,
+    image126,
+    image127,
+    image128,
+    image129,
+    image130,
+    image131,
+    image132,
+    image133,
+    image134,
+    image135,
+    image136,
+    image137,
+    image138,
+    image139,
+    image140,
+    image141,
+    image142,
+
+    image72,
+
+    image75,
+    image76,
+    image77,
+    image78,
+    image79,
+    image80,
+    image81,
+    image82,
+    image83,
+    image84,
+    image85,
+    image86,
+    image87,
+    image88,
+    image89,
+    image90,
+    image91,
+    image92,
+
+    image11,
+    image12,
+    image13,
+    image14,
+    image15,
+    image16,
+    image17,
+    image18,
+    image19,
+    image20,
+    image22,
+    image23,
+    image24,
+    image25,
+    image26,
+    image27,
+    image28,
+    image29,
+    image30,
+    image31,
+
+    image103,
+    image104,
+    image105,
+
+    image107,
+    image108,
+    image109,
+    image110,
+
+    image98,
+    image99,
+    image100,
+
+    image60,
+    image61,
+
+    image113,
+
+    image114,
+    image115,
+    image116,
+    image117,
+    image118,
+
+    image93,
+
+    image66,
+
+    image111,
+    image112,
+
+    image94,
+    image95,
+    image96,
+    image97,
+
+    image102,
+
+    image106,
+
+    image1,
+    image2,
+    image3,
+    image4,
+
+    image5,
+
+    image6,
+    image7,
+    image8,
+    image9,
+    image10,
+
+    image101,
+
+    image73,
+    image74,
+
+    image32,
+    image33,
+    image34,
+    image35,
+    image36,
+    image37,
+    image38,
+    image39,
+    image40,
+    image41,
+    image42,
+    image43,
+    image44,
+    image45,
+    image46,
+    image47,
+    image48,
+    image49,
+    image50,
+    image51,
+    image52,
+    image53,
+  ];
+
+
+
+
+  const gradientBackground = {
+    background: 'linear-gradient(90deg, #909090, #C0C0C0)'   
+  };
+
+  return (
+    <div className="min-h-screen" style={gradientBackground}>
+      <div className="text-center pt-10">
+        <img
+          src={Nihad_Logo}
+          className="w-[120px] align-middle justify-center m-auto mb-2"
+          alt=""
+        />
+        <h1 className="text-3xl sm:text-4xl md:text-5xl lg:text-6xl font-bold underline">Portfolio-Gallery</h1>
+      </div>
+      <div className="flex flex-wrap p-4 align-middle justify-center">
+        {images.map((image, index) => (
+          <a
+            key={index}
+            href={imageLinks[index]}
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <img
+              key={index}
+              src={image}
+              alt={`Image ${index}`}
+              className="w-[95%] align-middle justify-center m-auto h-auto rounded-xl transition-transform hover:scale-105 hover:shadow-lg my-5 border-b-[10px]"
+            />
+          </a>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default App;
